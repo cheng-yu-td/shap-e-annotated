713ec3241044146dd488fce0c06cb5631e47d281
Cheng <cheng.yu@trellisdata.com.au>
2023-06-15 15:21:45 +1000

    some notes and annotations

--- shap_e/rendering/mesh.py
+++ shap_e/rendering/mesh.py
@@ -23,6 +23,9 @@ class TriMesh:
     normals: Optional[np.ndarray] = None
 
     # Extra data per vertex and face.
+    # field(default_factory=dict):
+    # This part of the line sets the default value for vertex_channels using the field function from the dataclasses module.
+    # In this case, the default value is an empty dictionary ({}) created by calling the dict function.
     vertex_channels: Optional[Dict[str, np.ndarray]] = field(default_factory=dict)
     face_channels: Optional[Dict[str, np.ndarray]] = field(default_factory=dict)
 
@@ -66,6 +69,9 @@ class TriMesh:
             obj_dict = dict(verts=self.verts, faces=self.faces)
             if self.normals is not None:
                 obj_dict["normals"] = self.normals
+            # By convention, the author has chosen to use the prefixes "v_" and "f_" to identify different types of data in the file.
+            # This helps in distinguishing between vertex-related data and face-related data,
+            # making it easier to process and assign them to the appropriate variables (vertex_channels and face_channels, respectively).
             for k, v in self.vertex_channels.items():
                 obj_dict[f"v_{k}"] = v
             for k, v in self.face_channels.items():
@@ -87,21 +93,50 @@ class TriMesh:
             faces=self.faces,
         )
 
-    def write_obj(self, raw_f: BinaryIO):
+    def write_obj(self, raw_f: BinaryIO) -> None:
+        """
+        Writes the mesh data in OBJ format to a binary file.
+
+        Args:
+            raw_f (BinaryIO): Binary file object to write the OBJ data to.
+        """
+
         if self.has_vertex_colors():
+            # If the mesh has vertex colors, create vertex color data
             vertex_colors = np.stack([self.vertex_channels[x] for x in "RGB"], axis=1)
             vertices = [
                 "{} {} {} {} {} {}".format(*coord, *color)
                 for coord, color in zip(self.verts.tolist(), vertex_colors.tolist())
             ]
         else:
+            # If the mesh doesn't have vertex colors, only create vertex position data
             vertices = ["{} {} {}".format(*coord) for coord in self.verts.tolist()]
 
+        # Convert the faces into OBJ format
+        """
+        
+The + 1 operation in the line "f {} {} {}".format(str(tri[0] + 1), str(tri[1] + 1), str(tri[2] + 1)) 
+is used to adjust the indices of the vertices in the OBJ format.
+
+In the OBJ file format, vertex indices start from 1, rather than 0 as in many programming languages. 
+Therefore, when writing the face data to the OBJ file, the indices of the vertices need to be incremented by 1 to match the OBJ format's indexing convention.
+
+The tri variable in the line represents a triplet of vertex indices that form a face. 
+By adding 1 to each index (tri[0] + 1, tri[1] + 1, tri[2] + 1), the vertex indices are adjusted accordingly before being formatted into the OBJ face format string.
+
+For example, if a face has vertex indices [0, 1, 2], the line "f {} {} {}".format(str(tri[0] + 1), 
+str(tri[1] + 1), str(tri[2] + 1)) will format it as "f 1 2 3", which conforms to the OBJ format's indexing scheme.
+
+This adjustment ensures that when the OBJ file is read by other software or parsers that follow the OBJ specification, 
+the vertices will be correctly referenced by their indices, starting from 1.
+        """
         faces = [
             "f {} {} {}".format(str(tri[0] + 1), str(tri[1] + 1), str(tri[2] + 1))
             for tri in self.faces.tolist()
         ]
 
+        # Combine the vertex and face data
         combined_data = ["v " + vertex for vertex in vertices] + faces
 
+        # Write the combined data to the file
         raw_f.writelines("\n".join(combined_data))
--- shap_e/util/data_util.py
+++ shap_e/util/data_util.py
@@ -16,23 +16,28 @@ from shap_e.util.collections import AttrDict
 from shap_e.util.image_util import center_crop, get_alpha, remove_alpha, resize
 
 
+# Define a function that creates a batch of multimodal data. This can be used for tasks like training a machine learning model.
 def load_or_create_multimodal_batch(
-    device: torch.device,
-    *,
-    mesh_path: Optional[str] = None,
-    model_path: Optional[str] = None,
-    cache_dir: Optional[str] = None,
-    point_count: int = 2**14,
-    random_sample_count: int = 2**19,
-    pc_num_views: int = 40,
-    mv_light_mode: Optional[str] = None,
-    mv_num_views: int = 20,
-    mv_image_size: int = 512,
-    mv_alpha_removal: str = "black",
-    verbose: bool = False,
+        device: torch.device,
+        *,
+        mesh_path: Optional[str] = None,
+        model_path: Optional[str] = None,
+        cache_dir: Optional[str] = None,
+        point_count: int = 2 ** 14,
+        random_sample_count: int = 2 ** 19,
+        pc_num_views: int = 40,
+        mv_light_mode: Optional[str] = None,
+        mv_num_views: int = 20,
+        mv_image_size: int = 512,
+        mv_alpha_removal: str = "black",
+        verbose: bool = False,
 ) -> AttrDict:
-    if verbose:
+    # This function creates point cloud data, multiple views of a 3D model (if specified), and normalizes the data.
+
+    if verbose:  # If verbose mode is activated, print progress messages.
         print("creating point cloud...")
+
+    # Load or create point cloud from either a mesh file or a pre-trained 3D model.
     pc = load_or_create_pc(
         mesh_path=mesh_path,
         model_path=model_path,
@@ -42,87 +47,128 @@ def load_or_create_multimodal_batch(
         num_views=pc_num_views,
         verbose=verbose,
     )
+
+    # Concatenate point coordinates and color channels.
     raw_pc = np.concatenate([pc.coords, pc.select_channels(["R", "G", "B"])], axis=-1)
+
+    # Convert the numpy array to a PyTorch tensor, move it to the specified device and store it in the batch.
     encode_me = torch.from_numpy(raw_pc).float().to(device)
     batch = AttrDict(points=encode_me.t()[None])
-    if mv_light_mode:
+
+    if mv_light_mode:  # If a lighting mode for the multiview is specified, create multiview data.
         if verbose:
             print("creating multiview...")
         with load_or_create_multiview(
-            mesh_path=mesh_path,
-            model_path=model_path,
-            cache_dir=cache_dir,
-            num_views=mv_num_views,
-            extract_material=False,
-            light_mode=mv_light_mode,
-            verbose=verbose,
+                mesh_path=mesh_path,
+                model_path=model_path,
+                cache_dir=cache_dir,
+                num_views=mv_num_views,
+                extract_material=False,
+                light_mode=mv_light_mode,
+                verbose=verbose,
         ) as mv:
+            # Create empty lists to store multiview data.
             cameras, views, view_alphas, depths = [], [], [], []
-            for view_idx in range(mv.num_views):
+
+            for view_idx in range(mv.num_views):  # For each view in the multiview data:
+                # Load the view data. Channels include Red, Green, Blue, and potentially Alpha.
                 camera, view = mv.load_view(
                     view_idx,
                     ["R", "G", "B", "A"] if "A" in mv.channel_names else ["R", "G", "B"],
                 )
+
                 depth = None
-                if "D" in mv.channel_names:
-                    _, depth = mv.load_view(view_idx, ["D"])
-                    depth = process_depth(depth, mv_image_size)
+                if "D" in mv.channel_names:  # If depth channel is available:
+                    _, depth = mv.load_view(view_idx, ["D"])  # Load depth data.
+                    depth = process_depth(depth, mv_image_size)  # Process the depth data.
+
+                # Process the view image, handle alpha channel and resize the image.
                 view, alpha = process_image(
                     np.round(view * 255.0).astype(np.uint8), mv_alpha_removal, mv_image_size
                 )
+
+                # Process the camera image, center crop and resize it.
                 camera = camera.center_crop().resize_image(mv_image_size, mv_image_size)
+
+                # Add processed data to the corresponding lists.
                 cameras.append(camera)
                 views.append(view)
                 view_alphas.append(alpha)
                 depths.append(depth)
+
+            # Add the lists to the batch.
             batch.depths = [depths]
             batch.views = [views]
             batch.view_alphas = [view_alphas]
             batch.cameras = [cameras]
+
+    # Normalize the batch data and return it.
     return normalize_input_batch(batch, pc_scale=2.0, color_scale=1.0 / 255.0)
 
 
 def load_or_create_pc(
-    *,
-    mesh_path: Optional[str],
-    model_path: Optional[str],
-    cache_dir: Optional[str],
-    random_sample_count: int,
-    point_count: int,
-    num_views: int,
-    verbose: bool = False,
+        *,
+        mesh_path: Optional[str],
+        model_path: Optional[str],
+        cache_dir: Optional[str],
+        random_sample_count: int,
+        point_count: int,
+        num_views: int,
+        verbose: bool = False,
 ) -> PointCloud:
+    """
+    Function to load or create a point cloud (pc) from a given mesh or model path.
 
-    assert (model_path is not None) ^ (
-        mesh_path is not None
-    ), "must specify exactly one of model_path or mesh_path"
+    Args:
+        mesh_path (Optional[str]): The path to the mesh file. Either this or model_path should be provided.
+        model_path (Optional[str]): The path to the model file. Either this or mesh_path should be provided.
+        cache_dir (Optional[str]): The directory where cache files are stored. If not provided, caching is not used.
+        random_sample_count (int): The number of random points to sample from the multiview.
+        point_count (int): The number of points for the final point cloud.
+        num_views (int): The number of views to render for creating the multiview.
+        verbose (bool, optional): If True, prints out additional details. Defaults to False.
+
+    Returns:
+        PointCloud: The created or loaded point cloud.
+    """
+
+    # Ensure that either a mesh_path or model_path is provided, but not both
+    assert (model_path is not None) ^ (mesh_path is not None), "must specify exactly one of model_path or mesh_path"
+    # Decide which path to use
     path = model_path if model_path is not None else mesh_path
 
+    # If a cache_dir is provided
     if cache_dir is not None:
+        # Build the cache path
         cache_path = bf.join(
             cache_dir,
             f"pc_{bf.basename(path)}_mat_{num_views}_{random_sample_count}_{point_count}.npz",
         )
+        # If the cache file already exists, load it and return
         if bf.exists(cache_path):
             return PointCloud.load(cache_path)
     else:
         cache_path = None
 
+    # If the cache file does not exist, we create a new multiview and point cloud
     with load_or_create_multiview(
-        mesh_path=mesh_path,
-        model_path=model_path,
-        cache_dir=cache_dir,
-        num_views=num_views,
-        verbose=verbose,
-    ) as mv:
+            mesh_path=mesh_path,
+            model_path=model_path,
+            cache_dir=cache_dir,
+            num_views=num_views,
+            verbose=verbose,
+    ) as mv:  # this context manager either loads or creates a multiview
         if verbose:
             print("extracting point cloud from multiview...")
+        # Generate a point cloud from the multiview
         pc = mv_to_pc(
             multiview=mv, random_sample_count=random_sample_count, point_count=point_count
         )
+        # If a cache_dir is provided, save the point cloud for future use
         if cache_path is not None:
             pc.save(cache_path)
-        return pc
+        return pc  # return the point cloud
+
 
 
 @contextmanager
